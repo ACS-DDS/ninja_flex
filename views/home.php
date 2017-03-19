7dc6733a26568230c4fb5f47d7733703563185ce
CrackjY <yassine.latti@gmail.com>
2017-03-19 14:05:01 +0100

    hide image

--- views/home.php
+++ views/home.php
@@ -1,7 +1,7 @@
 <section id="all">			
 		<!-- <section id="block_bienvenue"> -->
 			<!-- <aside id="petite">	 -->
-				<img id="" class="" src="http://yassinl.dijon.codeur.online/achievo_project/inc/imgs/lololol.jpg"/>			
+				<!--<img id="" class="" src="http://yassinl.dijon.codeur.online/achievo_project/inc/imgs/lololol.jpg"/>-->			
 				<!-- <aside class="bienvenue">		 -->
 				<!-- </aside> -->
 				<!-- <aside class="c_home">				 -->
